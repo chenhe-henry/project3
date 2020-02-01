430ecc0d2e0f55b76bedb9b0119af17f98571d13
chenhe-henry <chenhe.elec@gmail.com>
2020-02-01 23:05:41 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production"
+});
+
+app.get("/movies", (req, res) => {
+  res.send({ count: 0, rows: [] });
+});
+
+app.get("/api/movies/:id", async (req, res) => {
+  const client = await pool.connect();
+  try {
+    const result = await client.query(
+      `SELECT * FROM MOVIES WHERE ID=${req.params.id}`
+    );
+    res.send({ ...result.rows[0] });
+  } catch (error) {
+    console.error(
+      `Could not retrieve movie with id ${req.params.id} from db`,
+      error
+    );
+    res.send({});
+  }
+  client.release();
+});
+
+app.get("/api/movies", async (req, res) => {
+  const client = await pool.connect();
+  try {
+    const count = await client.query("SELECT COUNT(1) as total from MOVIES");
+    const result = await client.query(
+      `SELECT id, movie_title, imdb_score FROM MOVIES order by imdb_score desc LIMIT 25 OFFSET ${req.query.offset}`
+    );
+    res.send({ count: count.rows[0].total, rows: result.rows });
+  } catch (error) {
+    console.error("Could not retrieve movies from db", error);
+    res.send({ count: 0, rows: [] });
+  }
+  client.release();
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
---
+++ web/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import MovieList from "./MovieList";
+import MovieDetails from "./MovieDetails";
+
+export default function App() {
+  return (
+    <Router>
+      <div className="app">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/movies">Movie List</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/movies" component={MovieList} />
+          <Route exact path="/">
+            <h2>Welcome to the Movie app!</h2>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
