d5349828545450a6639a9250a5c0c08478fb9c96
chenhe-henry <chenhe.elec@gmail.com>
2020-02-02 12:35:00 +1100

    apply scss

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "./App.css";
+import "./scss/App.scss";
 import MovieList from "./MovieList";
 import MovieDetails from "./MovieDetails";
 
