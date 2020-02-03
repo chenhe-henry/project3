8c306b7791ae475cad9151aec101c91ef8596c5e
chenhe-henry <chenhe.elec@gmail.com>
2020-02-04 00:21:36 +1100

    add logo for web

--- web/src/components/PropertyList/PropertyList.js
+++ web/src/components/PropertyList/PropertyList.js
@@ -12,18 +12,22 @@ class PropertyList extends React.Component {
           <img
             className="rent-house-img"
             src="https://rimh2.domainstatic.com.au/ZXahnYrndlCjReMN-uudzz724pQ=/fit-in/1920x1080/filters:format(jpeg):quality(80):no_upscale()/http://b.domainstatic.com.au.s3-website-ap-southeast-2.amazonaws.com/2016045005_1_1_200131_050507-w1920-h1280"
+            alt="pic-1"
           ></img>
           <img
             className="rent-house-img"
             src="https://rimh2.domainstatic.com.au/sG64XFCoCRAPgNTpao3kQUnCpPI=/fit-in/1920x1080/filters:format(jpeg):quality(80):no_upscale()/http://b.domainstatic.com.au.s3-website-ap-southeast-2.amazonaws.com/2016036221_3_1_200128_111059-w3000-h2000"
+            alt="pic-1"
           ></img>
           <img
             className="rent-house-img"
             src="https://rimh2.domainstatic.com.au/3QI7SGCiB9CHeSsr6SDOz5ZNAmU=/fit-in/1920x1080/filters:format(jpeg):quality(80):no_upscale()/http://b.domainstatic.com.au.s3-website-ap-southeast-2.amazonaws.com/2016038129_1_1_200129_045408-w1600-h1067"
+            alt="pic-1"
           ></img>
           <img
             className="rent-house-img"
             src="https://rimh2.domainstatic.com.au/2uP1cCMeOMK8FvPXz2zmLD45J5k=/fit-in/1920x1080/filters:format(jpeg):quality(80):no_upscale()/http://b.domainstatic.com.au.s3-website-ap-southeast-2.amazonaws.com/2016035172_3_1_200128_062702-w1920-h1440"
+            alt="pic-1"
           ></img>
         </div>
       </Fragment>
