07b99133098d678359959a88af82edce12c73b8d
chenhe-henry <chenhe.elec@gmail.com>
2020-02-04 21:28:11 +1100

    add home page link to every page

--- web/src/components/Navi/Navi.js
+++ web/src/components/Navi/Navi.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Navi.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 class Navi extends React.Component {
   state = {};
   render() {
     return (
       <div className="navigation">
         <h1 className="navigation-brand">
-          <strong>Domain</strong>
+          <Link to="/" className="navigation-brand-link">
+            <strong>Domain</strong>
+          </Link>
         </h1>
         <h2 className="navigation-title">
           Search Australia's home of property
