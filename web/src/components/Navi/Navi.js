7b8b0977290233fc9162f468d1878968356bc0a8
chenhe-henry <chenhe.elec@gmail.com>
2020-02-09 23:16:57 +1100

    udpate banner css

--- web/src/components/Navi/Navi.js
+++ web/src/components/Navi/Navi.js
@@ -6,23 +6,27 @@ class Navi extends React.Component {
   render() {
     return (
       <div className="banner">
-        <div className="container">
-          <div className="banner-brand">
-            <Link to="/" className="banner-brand-link">
+        <div className="banner-container">
+          <div className="banner-container-brand">
+            <Link to="/" className="banner-container-brand-link">
               <strong>Domain</strong>
             </Link>
           </div>
-          <div>Find a Property</div>
-          <div>Research</div>
-          <div>Find Agents</div>
-          <div>For Owners</div>
-          <div>Home Loans</div>
-          <div>Insurance</div>
-          <div>News</div>
-          <div>Commercial</div>
-          <div>Login</div>
+          <div className="banner-container-nav">
+            <div className="banner-container-nav-link">Find a Property</div>
+            <div className="banner-container-nav-link">Research</div>
+            <div className="banner-container-nav-link">Find Agents</div>
+            <div className="banner-container-nav-link">For Owners</div>
+            <div className="banner-container-nav-link">Home Loans</div>
+            <div className="banner-container-nav-link">Insurance</div>
+            <div className="banner-container-nav-link">News</div>
+            <div className="banner-container-nav-link">Commercial</div>
+            <div className="banner-container-nav-link login">Login</div>
+          </div>
+          <div className="banner-container-title">
+            Search Australia's home of property
+          </div>
         </div>
-        <h2 className="banner-title">Search Australia's home of property</h2>
       </div>
     );
   }
